97d17d8ab3de875792c6bf084376505117aca4bf
SA <SA@Vinz-MacBook-Pro.local>
2017-12-20 11:40:46 +0700

    initialize

---
+++ src/test/java/demo/Demo.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+    @Test
+    public void Demo () throws Exception{
+
+        Assert.assertEquals(0,0);
+
+    }
+
+}
